090718de0139f9ff59edc3d7e79cd54b0084e3cb
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-02-03 22:02:25 +0000

    update repositories

--- R/repositories.R
+++ R/repositories.R
@@ -1,3 +1,29 @@
+#' Display Bioconductor and CRAN snapshot repositories
+#'
+#' `repositories()` reports the URLs from which to install _Bioconductor_ and
+#' CRAN packages. There repositories are based on the data of the last build
+#' of the archived Bioconductor release.
+#'
+#' @details
+#' The CRAN repository reflects the `snapshot` arguments, which can be either
+#' `RSPM`, `MRAN`, or `CRAN`. The `CRAN` option will default to the established
+#' repository. For installation of archived packages on CRAN, see the
+#' [CRANinstall] function.
+#'
+#' @seealso [CRANinstall]
+#'
+#' @inheritParams BiocManager::repositories
+#' @inheritParams install
+#'
+#' @return A character vector of Bioconductor and CRAN repositories accounting
+#'   for previous releases
+#'
+#' @examples
+#' if (interactive()) {
+#'     # run within the Bioconductor 3.14 Docker container
+#'     repositories(version = "3.14", snapshot = "RSPM")
+#' }
+#'
 #' @export
 repositories <- function(
     site_repository = character(),
